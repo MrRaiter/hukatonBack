7f953db254143d8e0e94cae8514e69c9ab29e2a1
RodionLeginkov <rodion.leg.work@gmail.com>
2021-05-30 01:52:22 +0300

    Add my company

--- router/company/company-get.js
+++ router/company/company-get.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { promises: fs } = require('fs');
-const { Company } = require('../../models');
+const { Company, User } = require('../../models');
 const aunteficate = require('../../middlewares/aunteficate');
 
 router.get('/company', async (req, res, next) => {
@@ -8,6 +8,21 @@ router.get('/company', async (req, res, next) => {
     const companies = await Company.findAll();
 
     res.json(companies);
+  } catch (err) {
+    return res.status(403).json({ message: err });
+  }
+});
+router.get('/mycompany', aunteficate(), async (req, res, next) => {
+  try {
+    const userId = res.locals.user.id;
+    const companies = await Company.findAll({
+      include: [{
+        model: User,
+        where: { id: userId },
+      }],
+    });
+
+    res.json(...companies);
   } catch (err) {
     return res.status(403).json({ message: err });
   }
